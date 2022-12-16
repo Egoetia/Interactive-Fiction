a3cd163b97d70e98c0d5cfdc9c2e1618da159403
Egoetia <111281564+Egoetia@users.noreply.github.com>
2022-12-15 22:21:38 -0600

    Added basic stats and gui
    Just trying something here.

--- Interactive_Fiction.py
+++ Interactive_Fiction.py
@@ -1,3 +1,5 @@
+import PySimpleGUI as sg
+
 class Scene:
   def __init__(self, description, choices):
     self.description = description
@@ -8,24 +10,53 @@ class Choice:
     self.description = description
     self.result = result
 
-def start():
-  # Create a scene with a description and a list of choices
-  opening_scene = Scene("You are standing in a dark forest. You see a faint light in the distance.", [
-    Choice("Go towards the light.", go_towards_light),
-    Choice("Stay where you are.", stay)
-  ])
+class Game:
+  def __init__(self):
+    self.stats = {}
+  
+  def start(self):
 
-  # Display the scene and allow the player to make a choice
-  print(opening_scene.description)
-  for i, choice in enumerate(opening_scene.choices):
-    print(f"{i+1}. {choice.description}")
-  choice = int(input("Enter your choice: ")) - 1
-  opening_scene.choices[choice].result()
+    # Set a stat
+    game.set_stat("health", 100)
 
-def go_towards_light():
-  print("You follow the light and find your way out of the forest.")
+    # Get a stat
+    health = game.get_stat("health")
+    print(f"Your health is {health}.")
 
-def stay():
-  print("You decide to stay where you are and eventually starve to death.")
+    # Get a stat that hasn't been set
+    hunger = game.get_stat("hunger")
+    print(f"Your hunger is {hunger}.")
 
-start()
+    # Create a scene with a description and a list of choices
+    opening_scene = Scene("You are standing in a dark forest. You see a faint light in the distance.", [
+      Choice("Go towards the light.", self.go_towards_light),
+      Choice("Stay where you are.", self.stay)
+    ])
+
+    # Display the scene and allow the player to make a choice
+    layout = [
+      [sg.Text(opening_scene.description)],
+      *[[sg.Button(choice.description, key=i)] for i, choice in enumerate(opening_scene.choices)]
+    ]
+    window = sg.Window('Interactive Fiction Game', layout)
+    while True:
+      event, values = window.read()
+      if event == sg.WIN_CLOSED:
+        break
+      opening_scene.choices[int(event)].result()
+    window.close()
+
+  def go_towards_light(self):
+    print("You follow the light and find your way out of the forest.")
+
+  def stay(self):
+    print("You decide to stay where you are and eventually starve to death.")
+
+  def set_stat(self, stat, value):
+    self.stats[stat] = value
+  
+  def get_stat(self, stat):
+    return self.stats.get(stat, 0)
+
+game = Game()
+game.start()
